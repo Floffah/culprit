09a056e4ef64de25a9eaba52c36922b1de7d7855
Floffah <floffah@floffah.dev>
2026-05-25 00:16:14 +0100

    chore: ignore dist folder & default cleanup script

--- cleanup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-## Recipe: Culprit
-
-# Reason: Remove config files and installed recipes when uninstalling culprit.
-# Matcher: $HOME/.culprit
-# Size: 37.0 B
-rm -rf -- '/Users/ramsay/.culprit'
